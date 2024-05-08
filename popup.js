dc79212e40a5b1099fb6bfb6383648b7726a9fe4
Stephen Muema <105632674+Stephen11221@users.noreply.github.com>
2024-05-08 10:59:25 +0300

    Add files via upload

--- popup.js
+++ popup.js
@@ -10,3 +10,13 @@ function closePopup(){
     popup.classList.remove("open-popup");        
 }
 
+/*Create poppup*/
+let create =document.getElementById("create");
+
+function creatAcc(){
+    create.classList.add("create-open");
+}
+
+function closeCreate(){
+    create.classList.remove("create-open");
+}
